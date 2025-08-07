fd8874187f533bf841dc8be5f43d2a422982f04a
martindm <martindm.dev@gmail.com>
2025-08-07 23:47:19 +0100

    State update

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -109,6 +109,7 @@ export default function Home() {
   const handleClearResults = (e?: React.MouseEvent) => {
     if (e) e.preventDefault();
     setBooks([]);
+    setHasSearched(false);
     setSearchTerm('');
   };
 
